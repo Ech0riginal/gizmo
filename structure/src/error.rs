a14de8ae22cac2fbcd084d67796b26ae62b98fb3
Ech0riginal <samwatkins94@me.com>
2025-06-09 06:59:06 +0000

    error + lib

--- structure/src/error.rs
+++ structure/src/error.rs
@@ -1,19 +1,14 @@
-use bb8::RunError;
-
-use thiserror::Error;
-
 #[allow(clippy::large_enum_variant)]
-#[derive(Debug, Error)]
-pub enum GremlinError {
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
     #[error("data store disconnected: {0}")]
     Generic(String),
 
     #[error("The channel or socket has closed.")]
     Closed,
 
-    #[error(transparent)]
-    Websocket(#[from] tungstenite::Error),
-
+    // #[error(transparent)]
+    // Websocket(#[from] tungstenite::Error),
     #[error("Tungstenite Error {0}")]
     WebsocketClone(String),
 
@@ -38,31 +33,31 @@ pub enum GremlinError {
     WebSocketHandshake(String),
     #[error("An error occurred while performing handshake: {0}")]
     WebSocketTlsHandshake(String),
-    #[error(transparent)]
-    ChannelSend(#[from] futures::channel::mpsc::SendError),
-    #[error(transparent)]
-    TokioChannelSend(#[from] tokio::sync::mpsc::error::SendError<tungstenite::Message>),
-    #[error(transparent)]
-    BrokenChannel(#[from] tokio::sync::oneshot::error::RecvError),
+    // #[error(transparent)]
+    // ChannelSend(#[from] futures::channel::mpsc::SendError),
+    // #[error(transparent)]
+    // TokioChannelSend(#[from] tokio::sync::mpsc::error::SendError<tungstenite::Message>),
+    // #[error(transparent)]
+    // BrokenChannel(#[from] tokio::sync::oneshot::error::RecvError),
     #[error(transparent)]
     Uuid(#[from] uuid::Error),
     #[error(transparent)]
     IO(#[from] std::io::Error),
-    #[error(transparent)]
-    Tls(#[from] rustls::Error),
-    #[error(transparent)]
-    Pem(#[from] rustls_pki_types::pem::Error),
+    // #[error(transparent)]
+    // Tls(#[from] rustls::Error),
+    // #[error(transparent)]
+    // Pem(#[from] rustls_pki_types::pem::Error),
     #[error("Timed out while making connection")]
     ConnectionTimeout,
-    #[error(transparent)]
-    Parse(#[from] crate::io::Error),
+    // #[error(transparent)]
+    // Parse(#[from] crate::io::Error),
 }
 
-impl From<bb8::RunError<Self>> for GremlinError {
-    fn from(value: RunError<Self>) -> Self {
-        match value {
-            RunError::User(e) => e,
-            RunError::TimedOut => Self::ConnectionTimeout,
-        }
-    }
-}
+// impl From<bb8::RunError<Self>> for Error {
+//     fn from(value: RunError<Self>) -> Self {
+//         match value {
+//             RunError::User(e) => e,
+//             RunError::TimedOut => Self::ConnectionTimeout,
+//         }
+//     }
+// }
