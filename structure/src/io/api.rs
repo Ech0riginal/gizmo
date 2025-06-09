657f4d82df8fc4f5dd0765e7929e978ee98932db
Ech0riginal <samwatkins94@me.com>
2025-06-09 06:58:28 +0000

    io integrations

---
+++ structure/src/io/api.rs
@@ -0,0 +1,91 @@
+use crate::GID;
+use crate::GValue;
+use crate::io::{Error, Request, Response};
+use serde_json::Value;
+
+#[allow(private_bounds)]
+pub trait GremlinIO
+where
+    Self: 'static,
+    Self: Sealed,
+    Self: Send + Sync + Clone,
+    Self: Deserializer<Response> + Serializer<Request>,
+    Self: Deserializer<GValue> + Serializer<GValue>,
+    Self: Deserializer<GID> + Serializer<GID>,
+{
+    #[allow(nonstandard_style)]
+    const version: &'static str;
+
+    #[allow(nonstandard_style)]
+    const mime: &'static str;
+}
+
+pub trait Sealed {}
+
+pub trait Deserialize: Sized {
+    fn deserialize<V, T>(&self) -> Result<T, Error>
+    where
+        V: Deserializer<T>;
+}
+
+pub trait Deserializer<T>: Sealed {
+    fn deserialize(val: &Value) -> Result<T, Error>;
+}
+
+pub trait Serialize: Sized {
+    fn serialize<S>(&self) -> Result<Value, Error>
+    where
+        S: Serializer<Self>;
+}
+
+pub trait Serializer<T>: Sealed {
+    fn serialize(val: &T) -> Result<Value, Error>;
+}
+
+pub trait IOHelpers {
+    fn get<'a>(value: &'a Value, key: &'static str) -> Result<&'a Value, Error> {
+        value.get(key).ok_or(Error::missing(key))
+    }
+}
+
+mod blankets {
+    use crate::io::api::{Deserialize, Serialize};
+    use crate::io::{Deserializer, Error, Serializer};
+    use serde_json::Value;
+
+    impl<T> Serialize for T {
+        fn serialize<S>(&self) -> Result<Value, Error>
+        where
+            S: Serializer<Self>,
+        {
+            S::serialize(self)
+        }
+    }
+
+    impl Deserialize for Value {
+        fn deserialize<V, T>(&self) -> Result<T, Error>
+        where
+            V: Deserializer<T>,
+        {
+            V::deserialize(&self).map(|a| a.into())
+        }
+    }
+
+    // impl<T, G> Serializer<Box<T>> for G
+    // where
+    //     G: Serializer<T>,
+    // {
+    //     fn serialize(val: &Box<T>) -> Result<Value, Error> {
+    //         (*val).serialize::<G>()
+    //     }
+    // }
+    //
+    // impl<T, G> Deserializer<Box<T>> for G
+    // where
+    //     G: Deserializer<T>,
+    // {
+    //     fn deserialize(val: &Value) -> Result<Box<T>, Error> {
+    //         val.deserialize::<G, T>().map(Box::new)
+    //     }
+    // }
+}
