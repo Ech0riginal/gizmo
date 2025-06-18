015c0f31b69e230eb0533316197dccdf385a1974
Ech0riginal <samwatkins94@me.com>
2025-06-18 18:23:18 +0000

    gv2 bytecode impl

--- io/src/graphson/v2/process/bytecode.rs
+++ io/src/graphson/v2/process/bytecode.rs
@@ -1,8 +1,58 @@
 use crate::graphson::prelude::*;
 
 impl<D: Dialect> GraphsonDeserializer<Bytecode, D> for GraphSON<V2> {
-    fn deserialize(_val: &Value) -> Result<Bytecode, Error> {
-        todo!()
+    fn deserialize(val: &Value) -> Result<Bytecode, Error> {
+        let obj = get_value!(val, Value::Object)?;
+        let mut source_instructions = list![];
+        let mut step_instructions = list![];
+
+        if let Some(sourceroonies) = obj.get("source") {
+            source_instructions.extend(
+                get_value!(sourceroonies, Value::Array)?
+                    .iter()
+                    .map(|v| v.deserialize::<Self, D, Instruction>()).collect::<Result<List<_>, Error>>()?
+                    .drain(..)
+            )
+        }
+
+        if let Some(steperoonies) = obj.get("step") {
+            step_instructions.extend(
+                get_value!(steperoonies, Value::Array)?
+                    .iter()
+                    .map(|v| v.deserialize::<Self, D, Instruction>()).collect::<Result<List<_>, Error>>()?
+                    .drain(..)
+            )
+        }
+
+
+        Ok(Bytecode {
+            source_instructions,
+            step_instructions,
+        })
+    }
+}
+
+impl<D: Dialect> GraphsonDeserializer<Instruction, D> for GraphSON<V2> {
+    fn deserialize(val: &Value) -> Result<Instruction, Error> {
+        let arr = get_value!(val, Value::Array)?;
+        if arr.is_empty() {
+            return Err(Error::unexpected(val, "an array with a size of at least 1"))
+        }
+
+        let op = arr[0].deserialize::<Self, D, String>()?;
+        let args = match arr.len() {
+            1 => list![],
+            2 => list![arr[1].deserialize::<Self, D, GValue>()?],
+            _ => arr[1..]
+                .iter()
+                .map(|arg| arg.deserialize::<Self, D, GValue>())
+                .collect::<Result<List<GValue>, Error>>()?,
+        };
+
+        Ok(Instruction {
+            op,
+            args
+        })
     }
 }
 
@@ -13,7 +63,7 @@ impl<D: Dialect> GraphsonSerializer<Bytecode, D> for GraphSON<V2> {
             .iter()
             .map(|m| {
                 let mut instruction = vec![];
-                instruction.push(Value::String(m.operator.clone()));
+                instruction.push(Value::String(m.op.clone()));
 
                 let arguments: Result<Vec<Value>, Error> =
                     m.args.iter().map(|a| a.serialize::<Self, D>()).collect();
@@ -27,7 +77,7 @@ impl<D: Dialect> GraphsonSerializer<Bytecode, D> for GraphSON<V2> {
             .iter()
             .map(|m| {
                 let mut instruction = vec![];
-                instruction.push(Value::String(m.operator.clone()));
+                instruction.push(Value::String(m.op.clone()));
 
                 let arguments: Result<Vec<Value>, Error> =
                     m.args.iter().map(|a| a.serialize::<Self, D>()).collect();
