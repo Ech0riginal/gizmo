a3ab0445698a4611d0b3be92390e0f4754b4b7a3
Ech0riginal <samwatkins94@me.com>
2025-06-21 01:17:33 +0000

    gv2 scope

--- gizmo-io/src/formats/graphson/v2/process/scope.rs
+++ gizmo-io/src/formats/graphson/v2/process/scope.rs
@@ -1,16 +1,21 @@
 use crate::formats::graphson::prelude::*;
 
 impl<D: Dialect> GraphsonDeserializer<Scope, D> for GraphSON<V2> {
-    fn deserialize(_val: &Value) -> Result<Scope, Error> {
-        todo!()
+    fn deserialize(val: &Value) -> Result<Scope, Error> {
+        let repr = val.deserialize::<Self, D, String>()?;
+        match repr.as_str() {
+            Scope::GLOBAL => Ok(Scope::Global),
+            Scope::LOCAL => Ok(Scope::Local),
+            _ => Err(Error::unexpected(val, "'global' or 'local'")),
+        }
     }
 }
 
 impl<D: Dialect> GraphsonSerializer<Scope, D> for GraphSON<V2> {
     fn serialize(val: &Scope) -> Result<Value, Error> {
         Ok(json!(match val {
-            Scope::Global => "global",
-            Scope::Local => "local",
+            Scope::Global => Scope::GLOBAL,
+            Scope::Local => Scope::LOCAL,
         }))
     }
 }
