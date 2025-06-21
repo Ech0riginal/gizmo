039bac1c4cc5fa23a2070939bb198ca92b896e53
Ech0riginal <samwatkins94@me.com>
2025-06-21 01:17:53 +0000

    gv2 textp

--- gizmo-io/src/formats/graphson/v2/process/textp.rs
+++ gizmo-io/src/formats/graphson/v2/process/textp.rs
@@ -1,15 +1,43 @@
 use crate::formats::graphson::prelude::*;
 
 impl<D: Dialect> GraphsonDeserializer<TextP, D> for GraphSON<V2> {
-    fn deserialize(_val: &Value) -> Result<TextP, Error> {
-        todo!()
+    fn deserialize(val: &Value) -> Result<TextP, Error> {
+        let map = get_value!(val, Value::Object)?;
+        let value = map
+            .ensure("value")?
+            .deserialize::<Self, D, GValue>()?
+            .boxed();
+        let predicate = map.ensure("predicate")?.deserialize::<Self, D, Text>()?;
+        Ok(TextP { predicate, value })
+    }
+}
+
+impl<D: Dialect> GraphsonDeserializer<Text, D> for GraphSON<V2> {
+    fn deserialize(val: &Value) -> Result<Text, Error> {
+        let repr = val.deserialize::<Self, D, String>()?;
+        match repr.as_str() {
+            Text::CONTAINING => Ok(Text::Containing),
+            Text::ENDING_WITH => Ok(Text::EndingWith),
+            Text::STARTING_WITH => Ok(Text::StartingWith),
+            Text::NOT_CONTAINING => Ok(Text::NotContaining),
+            Text::NOT_ENDING_WITH => Ok(Text::NotEndingWith),
+            Text::NOT_STARTING_WITH => Ok(Text::NotStartingWith),
+            _ => Err(Error::unexpected(val, "a valid Text predicate")),
+        }
     }
 }
 
 impl<D: Dialect> GraphsonSerializer<TextP, D> for GraphSON<V2> {
     fn serialize(val: &TextP) -> Result<Value, Error> {
         Ok(json!({
-            "predicate" : val.operator(),
+            "predicate" : match val.predicate {
+                Text::Containing => Text::CONTAINING,
+                Text::EndingWith => Text::ENDING_WITH,
+                Text::StartingWith => Text::STARTING_WITH,
+                Text::NotContaining => Text::NOT_CONTAINING,
+                Text::NotEndingWith => Text::NOT_ENDING_WITH,
+                Text::NotStartingWith => Text::NOT_STARTING_WITH,
+            },
             "value" : val.value().serialize::<Self, D>()?
         }))
     }
