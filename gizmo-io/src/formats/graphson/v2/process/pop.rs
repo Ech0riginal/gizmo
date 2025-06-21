675fb1a30af96d6e73370ead754525a7e6686b5e
Ech0riginal <samwatkins94@me.com>
2025-06-21 01:17:24 +0000

    gv2 pop

--- gizmo-io/src/formats/graphson/v2/process/pop.rs
+++ gizmo-io/src/formats/graphson/v2/process/pop.rs
@@ -1,18 +1,25 @@
 use crate::formats::graphson::prelude::*;
 
 impl<D: Dialect> GraphsonDeserializer<Pop, D> for GraphSON<V2> {
-    fn deserialize(_val: &Value) -> Result<Pop, Error> {
-        todo!()
+    fn deserialize(val: &Value) -> Result<Pop, Error> {
+        let repr = val.deserialize::<Self, D, String>()?;
+        match repr.as_str() {
+            Pop::ALL => Ok(Pop::All),
+            Pop::FIRST => Ok(Pop::First),
+            Pop::LAST => Ok(Pop::Last),
+            Pop::MIXED => Ok(Pop::Mixed),
+            _ => Err(Error::unexpected(val, "a valid Pop value")),
+        }
     }
 }
 
 impl<D: Dialect> GraphsonSerializer<Pop, D> for GraphSON<V2> {
     fn serialize(val: &Pop) -> Result<Value, Error> {
         Ok(json!(match val {
-            Pop::All => "all",
-            Pop::First => "first",
-            Pop::Last => "last",
-            Pop::Mixed => "mixed",
+            Pop::All => Pop::ALL,
+            Pop::First => Pop::FIRST,
+            Pop::Last => Pop::LAST,
+            Pop::Mixed => Pop::MIXED,
         }))
     }
 }
