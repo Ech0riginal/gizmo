d5be80dd29dee1c217aa4227ae8f16f0c56009ee
Ech0riginal <samwatkins94@me.com>
2025-07-08 03:46:37 +0000

    EZ dialect serde

---
+++ gizmo-io/src/formats/graphson/v3/aux/gvalue/janus.rs
@@ -0,0 +1,48 @@
+use super::serde;
+use crate::dialects::Janus;
+use crate::formats::graphson::prelude::*;
+
+serde!(
+    Janus,
+    Class -> Class,
+    Date -> Date,
+    Double -> Double,
+    Float -> Float,
+    Integer -> Integer,
+    List -> List<GValue>,
+    Long -> Long,
+    Map -> Map<GValue, GValue>,
+    Set -> Set,
+    Timestamp -> Timestamp,
+    Uuid -> Uuid,
+
+    Edge -> Edge,
+    Path -> Path,
+    Property -> Property,
+    TinkerGraph -> TinkerGraph,
+    Vertex -> Vertex,
+    VertexProperty -> VertexProperty,
+
+    Barrier -> Barrier,
+    Binding -> Binding,
+    BulkSet -> BulkSet,
+    Bytecode -> Bytecode,
+    Cardinality -> Cardinality,
+    Column -> Column,
+    Direction -> Direction,
+    Lambda -> Lambda,
+    Merge -> Merge,
+    Metrics -> Metrics,
+    Operator -> Operator,
+    Order -> Order,
+    P -> P,
+    Pick -> Pick,
+    Pop -> Pop,
+    Scope -> Scope,
+    T -> T,
+    TextP -> TextP,
+    TraversalMetrics -> TraversalMetrics,
+    Traverser -> Traverser,
+
+    Geometry -> Geometry,
+);
